fe3554a770fbe0d79c123f1f781f62d945375c04
usasergio <usasergio2@gmail.com>
2022-08-04 17:37:52 +0200

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 //$config['base_url'] = 'https://app.esvirgua.com/evg';
 $config['base_url'] = 'http://localhost/backend';
 
-
+$config['debug'] = true;
 
 /*
 |--------------------------------------------------------------------------
